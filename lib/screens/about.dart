e979d5096c4ea5a355c86488e3345d771344f486
Pc <ahmedmf9549@gmail.com>
2019-09-09 16:51:26 +0200

    cccc

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
 class About extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
